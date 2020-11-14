1faafced479ec4ac8a2003b2bbd9cc8540a0413f
AsayamaRyuga <keep.g.dragon12@gmail.com>
2020-11-14 12:12:09 +0900

    test1

---
+++ Ayas_realTimeChart-ver1/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Ayas_realTimeChart_ver1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button_Connect = new System.Windows.Forms.Button();
+            this.button_Disconnect = new System.Windows.Forms.Button();
+            this.label_Free = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(396, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(489, 411);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // button_Connect
+            // 
+            this.button_Connect.Location = new System.Drawing.Point(46, 12);
+            this.button_Connect.Name = "button_Connect";
+            this.button_Connect.Size = new System.Drawing.Size(102, 32);
+            this.button_Connect.TabIndex = 1;
+            this.button_Connect.Text = "Connect";
+            this.button_Connect.UseVisualStyleBackColor = true;
+            this.button_Connect.Click += new System.EventHandler(this.button_Connect_Click);
+            // 
+            // button_Disconnect
+            // 
+            this.button_Disconnect.Location = new System.Drawing.Point(154, 12);
+            this.button_Disconnect.Name = "button_Disconnect";
+            this.button_Disconnect.Size = new System.Drawing.Size(102, 32);
+            this.button_Disconnect.TabIndex = 2;
+            this.button_Disconnect.Text = "Disconnect";
+            this.button_Disconnect.UseVisualStyleBackColor = true;
+            this.button_Disconnect.Click += new System.EventHandler(this.button_Disconnect_Click);
+            // 
+            // label_Free
+            // 
+            this.label_Free.AutoSize = true;
+            this.label_Free.Location = new System.Drawing.Point(102, 408);
+            this.label_Free.Name = "label_Free";
+            this.label_Free.Size = new System.Drawing.Size(69, 15);
+            this.label_Free.TabIndex = 3;
+            this.label_Free.Text = "Free label";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1, 50);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(389, 316);
+            this.textBox1.TabIndex = 4;
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM5";
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(895, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label_Free);
+            this.Controls.Add(this.button_Disconnect);
+            this.Controls.Add(this.button_Connect);
+            this.Controls.Add(this.chart1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button_Connect;
+        private System.Windows.Forms.Button button_Disconnect;
+        private System.Windows.Forms.Label label_Free;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.IO.Ports.SerialPort serialPort1;
+    }
+}
+
---
+++ Ayas_realTimeChart-ver1/Form1.cs
@@ -0,0 +1,106 @@
+﻿//test git
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Diagnostics;
+
+namespace Ayas_realTimeChart_ver1
+{
+    public partial class Form1 : Form
+    {
+        string element;
+        static Stopwatch sw = new Stopwatch();
+        string timeStamp;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            timeStamp = Convert.ToString(Math.Round((double)sw.ElapsedTicks / Stopwatch.Frequency , 1));
+            element = timeStamp + "," + serialPort1.ReadLine();
+            this.Invoke(new EventHandler(DisplayText));
+            this.Invoke(new EventHandler(showChart));
+        }
+
+        private void button_Connect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.Open();
+                sw.Restart();//stopwatchスタート
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button_Disconnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void DisplayText(object sender, EventArgs e)
+        {
+            textBox1.AppendText(element + Environment.NewLine);
+        }
+
+        private void showChart(object sender, EventArgs e)
+        {
+            string legend1 = "Series1";
+            if (element.Length >= 5)
+            {
+                string[] strArrayData = element.Split(',');
+                double y = Convert.ToDouble(strArrayData[1]);
+                double x = Convert.ToDouble(strArrayData[0]);
+                //double y = double.Parse(strArrayData[1]);
+                //double x = double.Parse(strArrayData[0]);
+                chart1.Series[legend1].Points.AddXY(x, y);
+                chart1.ChartAreas[0].AxisX.Maximum = x ;
+                chart1.ChartAreas[0].AxisX.Minimum = x - 10;
+            }
+            
+            chart1.Series[legend1].IsVisibleInLegend = false;// 凡例表示設定
+            chart1.Series[legend1].IsValueShownAsLabel = false;// データラベル表示設定
+            chart1.Series[legend1].ChartType = SeriesChartType.Line;// 折れ線グラフを指定
+            chart1.Series[legend1].BorderWidth = 2;// 折れ線グラフの幅を指定
+            chart1.Series[legend1].Color = Color.FromArgb(243, 152, 0);// RGBでグラフの色を指定
+            chart1.ChartAreas[0].AxisY.Maximum = 8.3;
+            chart1.ChartAreas[0].AxisY.Minimum = 8;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            try
+            {
+                serialPort1.Close();
+                sw.Stop();
+            }
+            catch { }
+        }
+    }
+}
